9a85594ac0aaed33e45bf2688e6b608d10d44e5a
Wilson P <wilson.prevette@gmail.com>
2018-08-08 23:46:07 -0500

    created all directories

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (BURGER_NAME, DEVOURED)
+VALUES ("Chicken Bacon Ranch", true);
+
+INSERT INTO burgers (BURGER_NAME, DEVOURED)
+VALUES ("Guacamole Black Bean", true);
+
+INSERT INTO burgers (BURGER_NAME, DEVOURED)
+VALUES ("Bacon Cheddar", true);
---
+++ server.js
@@ -0,0 +1,4 @@
+//Dependencies
+
+var express = require("express");
+var bodyParser = require("body-parser");
