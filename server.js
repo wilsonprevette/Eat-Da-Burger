3da5b775f854c33fba5164c3864a5a759938bb68
Wilson P <wilson.prevette@gmail.com>
2018-08-12 01:33:32 -0500

    changes to server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 //Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const handlebars = require('express-handlebars');
+const app = express();
+const routes = require("./controllers/burgers_controller.js");
+const port = 8080;
 
-var express = require("express");
-var bodyParser = require("body-parser");
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("listening")
+};
